bc1f4ff079aa041bca8bdb9311a0b1d7137af202
Recursivex5137 <recursivex5137@gmail.com>
2018-02-05 00:35:04 -0500

    Added all functionality of the app using react. Including bootstrap 3, ytsearch, lodash, and completed the following components: search-bar, video-detail, video-list, and video-list-item.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
+
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
+
+import { YoutubeApiKey } from './Api-Keys';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      videos: [],  
+      selectedVideo: null,
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: YoutubeApiKey, term: term }, (videos) => {
+      this.setState({ 
+        videos: videos, 
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Youtube Search</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar onSearchChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
